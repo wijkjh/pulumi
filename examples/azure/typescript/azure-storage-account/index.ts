223c90d3c61ec1ed39d57e7cc1692088cb72493d
Henk Jan van Wijk <wijkjh@gmail.com>
2022-04-25 22:54:32 +0200

    Storage account example plus README

---
+++ examples/azure/typescript/azure-storage-account/index.ts
@@ -0,0 +1,32 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+import * as storage from "@pulumi/azure-native/storage";
+
+const contactPerson = "My name"
+const resourceGroupName = "rg-pulumi-demo"
+
+// Create an Azure Resource Group
+const resourceGroup = new resources.ResourceGroup("rg",
+    { 
+        resourceGroupName,
+        tags: {
+            "Contactperson": contactPerson
+        }
+    }, {
+        protect: true,
+    });
+
+
+// Create an Azure resource (Storage Account)
+const storageAccount = new storage.StorageAccount("sa", {
+    resourceGroupName: resourceGroup.name,
+    sku: {
+        name: storage.SkuName.Standard_LRS,
+    },
+    kind: storage.Kind.StorageV2,
+});
+
+// Export the primary key of the Storage Account
+const storageAccountKeys = pulumi.all([resourceGroup.name, storageAccount.name]).apply(([resourceGroupName, accountName]) =>
+    storage.listStorageAccountKeys({ resourceGroupName, accountName }));
+export const primaryStorageKey = storageAccountKeys.keys[0].value;
